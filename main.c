1696387c9aa91b0622e5c42ec98830f7552ec769
Ismail-Lh <ismailsaad03@gmail.com>
2023-04-25 15:41:12 +0100

    how to declare variables in C

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
-int main(){
-  printf("Hello world.\n");
-  printf("I like pizza!\n");
+int main() {
+  // *:VARIABLES -----> Variable is basically nothing but the name of a memory location that we use for storing data. We can change the value of a variable in C or any other language, and we can also reuse it multiple times. We use symbols in variables for representing the memory location- so that it becomes easily identifiable by any user.
+
+  int x; // !: declaration
+  x = 1; // !: initialization
+  int y = 2; // !: declaration & initialization
+
+  int age = 21; // !: integer
+  float gpa = 2.1; // !: float number
+  char grade = 'C'; // !: single character
+  char name[] = "ism"; // !: array of characters (string)
+
+  printf("Hello %s.\n", name);
+  printf("You are %d years old.\n", age);
+  printf("Your average grade is %c.\n", grade);
+  printf("Your gpa is %f.\n", gpa);
 
   return 0;
 }

1b44d987eaf5caf1f7e031ee76b13f67d67615e8
Ismail-Lh <ismailsaad03@gmail.com>
2023-04-25 18:34:24 +0100

    C data types

--- main.c
+++ main.c
@@ -1,21 +1,41 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
-  // *:VARIABLES -----> Variable is basically nothing but the name of a memory location that we use for storing data. We can change the value of a variable in C or any other language, and we can also reuse it multiple times. We use symbols in variables for representing the memory location- so that it becomes easily identifiable by any user.
+  //*: DATA TYPES
 
-  int x; // !: declaration
-  x = 1; // !: initialization
-  int y = 2; // !: declaration & initialization
+  char a = 'C'; // !: single character %c
+  char b[] = "Boo"; // !: array of characters %s
 
-  int age = 21; // !: integer
-  float gpa = 2.1; // !: float number
-  char grade = 'C'; // !: single character
-  char name[] = "ism"; // !: array of characters (string)
+  float c = 3.141592; // !: 4 bytes (32 bites of precision) 6 - 7 digits %f
+  double d = 3.141592653589793; // !: 8 bytes (64 bites of precision) 15 - 16 digits %lf
 
-  printf("Hello %s.\n", name);
-  printf("You are %d years old.\n", age);
-  printf("Your average grade is %c.\n", grade);
-  printf("Your gpa is %f.\n", gpa);
+  bool e = true; // !: 1 byte (true or false) %d
+
+  char f = 100; // !: 1 byte (-128 to +127) %d or %c 
+  unsigned char g = 255; // !: 1 byte (0 to +255) %d or %c
+
+  short int h = 32767; // !: 2 bytes (-32,768 to +32,767)
+  unsigned short int i = 65535; // !: 2 bytes (0 to 65,535)
+
+  int j = 2147483647; // !: 4 bytes (-2,147,483,648 to +2,147,483,647) %d
+  unsigned int k = 4294967295; // !: 4 bytes (0 to 4294967295) %u
+
+  long long int l = 9223372036854775807; // !: 8 bytes (−9 223 372 036 854 775 807 to  +9 223 372 036 854 775 807) %lld
+  unsigned long long int m = 18446744073709551615U; // !: 8 bytes (0 to 18 446 744 073 709 551 615) %llu
+
+  printf("float: %f \n", c); // ?: float
+  printf("double: %0.15lf \n", d); // ?: double
+  printf("boolean: %d \n", e); // ?: boolean
+  printf("char as numeric value: %d \n", f); // ?: char as numeric value
+  printf("char as numeric value: %c \n", f); // ?: char as numeric value
+  printf("unsigned char as numeric value: %d \n", g); // ?: unsigned char as numeric value
+  printf("short: %d \n", h); // ?: short
+  printf("unsigned short: %d \n", i); // ?: unsigned short
+  printf("int: %d \n", j); // ?: int
+  printf("unsigned short: %u \n", k); // ?: unsigned short
+  printf("long long int: %lld \n", l); // ?: long long int
+  printf("unsigned long long int: %llu \n", m); // ?: unsigned long long int
 
   return 0;
 }

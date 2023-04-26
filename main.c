cbe28862c7c8536cdde830648e47abbcf8acb476
Ismail-Lh <ismailsaad03@gmail.com>
2023-04-26 14:27:06 +0100

    constant

--- main.c
+++ main.c
@@ -1,24 +1,11 @@
 #include <stdio.h>
 
 int main() {
-  // *:FORMAT SPECIFIER -----> defined and formats a type of data to be displayed
+  // *:CONSTANT -----> fixed value that cannot be altered by the programme during its execution
 
-  // ?: %c = character
-  // ?: %s = string (array of character)
-  // ?: %f = float
-  // ?: %lf = double
-  // ?: %d = integer
+  const float PI = 3.14159;
 
-  // ?: %.1 = decimal precision
-  // ?: %1 = minimum field width
-  // ?: %- = left align
-
-  float item1 = 5.75;
-  float item2 = 10.00;
-  float item3 = 100.99;
-
-  printf("Item 1: $%f \n", item1); // !: float format specifier
-  printf("Item 1: $%.2f \n", item1); // !: float format specifier with 2 decimal precision
+  printf("%f", PI);
 
   return 0;
 }

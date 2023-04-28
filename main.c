93bf8ab69d4e293a091ca9ed8eb7ba982d4f561a
Ismail-Lh <ismailsaad03@gmail.com>
2023-04-28 19:04:31 +0100

    while loop

--- main.c
+++ main.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-  // * FOR LOOP -----> repeats a section of code a limited amount of times
+  // * WHILE LOOP -----> repeats a section of code possibly unlimited amount of times WHILE some condition remains true, a while loop might not execute at all
 
-  for (int i = 1; i <= 10; i++)
+  char name[25];
+
+  printf("What's your name?: ");
+  fgets(name, 25, stdin);
+  name[strlen(name) - 1] = '\0';
+
+
+  while (strlen(name) == 0)
   {
-    printf("%d \n", i);
+    printf("\nYou did not enter your name\n");
+
+    printf("What's your name?: ");
+    fgets(name, 5, stdin);
+    name[strlen(name) - 1] = '\0';
   }
 
+  printf("Thank you %s", name);
 
   return 0;
 }
+

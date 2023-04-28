0a547065f6a72db224dda55871f7d93f1913f8a8
Ismail-Lh <ismailsaad03@gmail.com>
2023-04-28 18:44:35 +0100

    for loop

--- main.c
+++ main.c
@@ -1,30 +1,13 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
-  // * STRING FUNCTIONS
+  // * FOR LOOP -----> repeats a section of code a limited amount of times
 
-  char string1[32] = "Ismail";
-  char string2[32] = "Lahbiyeb";
+  for (int i = 1; i <= 10; i++)
+  {
+    printf("%d \n", i);
+  }
 
-  // strlwr(string1); // ?: converts a string to lowercase
-  // strupr(string1); // ?: converts a string to uppercase
-  // strcat(string1, string2); // ?: appends string2 to the end of string1
-  // strncat(string1, string2, 4); // ?: appends n characters from string2 to the end of string1
-  // strcpy(string1, string2); // ?: copy string2 to string1
-  //strncpy(string1, string2, 4); // ?: copy n characters from string2 to string1
-
-  // strset(string1, '?'); // ?: sets all characters of a string to a given character
-  // strnset(string1, '?', 1); // ?: sets first n character of a string to a given character
-  // strrev(string1); // ?: reverse a string
-
-  int result = strlen(string1); // ?: return the string length
-  // int result = strcmp(string1, string2); // ?: string compare all characters(check if the strings are the same)
-  // int result = strncmp(string1, string2, 1); // ?: string compare n characters
-  // int result = strcmpi(string1, string2); // ?: string compare all characters (check if the strings are the same) (ignore case sensitive)
-  // int result = strnicmp(string1, string2, 1); // ?: string compare n characters (ignore case sensitive)
-
-  printf("%d", result);
 
   return 0;
 }

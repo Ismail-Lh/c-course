b10db18c7c3c0ba2456a130e64b1dce0ad734910
Ismail-Lh <ismailsaad03@gmail.com>
2023-05-01 16:03:36 +0100

    print an array using for-loop

--- main.c
+++ main.c
@@ -5,9 +5,15 @@ int main() {
   // * ARRAY -----> a data structure that can store many values of the same data type
 
 
-  double prices[6] = { 5.0, 4.0, 10.0, 15.0 , 25.0, 20.0 };
+  double prices[] = { 5.0, 4.0, 10.0, 15.0 , 25.0, 20.0 };
 
-  printf("$%.2lf", prices[5]);
+  // !: GET THE LENGTH OF THE PRICES ARRAY
+  size_t pricesLen = sizeof(prices) / sizeof(prices[0]);
+
+  for (int i = 0; i < pricesLen; i++)
+  {
+    printf("$%.2lf \n", prices[i]);
+  }
 
   return 0;
 }
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+for (int i = 0; i < size; i++)
+  // {
+  //   printf("$%.2lf", prices[i]);
+  // }

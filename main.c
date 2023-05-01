b39eb25ad535587d258c35ab5067dccf256494d8
Ismail-Lh <ismailsaad03@gmail.com>
2023-05-01 17:27:38 +0100

    sort an array

--- main.c
+++ main.c
@@ -1,31 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 
+void sort(int array[], int size) {
+
+  for (int i = 0; i < size - 1; i++)
+  {
+    for (int j = 0; j < size - i - 1; j++)
+    {
+      if (array[j] > array[j + 1]) {
+        int temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+
+  }
+}
+
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; i++)
+  {
+    printf("%d ", array[i]);
+  }
+
+}
+
 int main() {
-  // * SWAP VALUE OF TWO VARIABLES
+  // * SORT AN ARRAY
 
-  // char x = 'X';
-  // char y = 'Y';
-  // char temp;
+  int array[] = { 9, 77, 1, 0 , 5, 2, 99, 3, 25, 8, 6, 4 };
+  int size = sizeof(array) / sizeof(array[0]);
 
-  // temp = x;
-  // x = y;
-  // y = temp;
-
-  // printf("x = %c \n", x);
-  // printf("y = %c \n", y);
-
-  char x[15] = "water";
-  char y[15] = "Lemonade";
-  char temp[15];
-
-  strcpy(temp, x);
-  strcpy(x, y);
-  strcpy(y, temp);
-
-  printf("x = %s \n", x);
-  printf("y = %s \n", y);
-
+  sort(array, size);
+  printArray(array, size);
 
   return 0;
 }

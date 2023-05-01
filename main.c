c9a2200b627d05bddc9e4955ac876978efa7a28e
Ismail-Lh <ismailsaad03@gmail.com>
2023-05-01 17:09:39 +0100

    swap values of two variables

--- main.c
+++ main.c
@@ -2,40 +2,29 @@
 #include <string.h>
 
 int main() {
-  // * 2D ARRAY -----> an array, where each element is an entire array
-  // * useful if you need a matrix, grid, or table of date 
+  // * SWAP VALUE OF TWO VARIABLES
 
-  int numbers[3][3] = { {1,2,3}, {4,5,6}, {7,8,9} };
+  // char x = 'X';
+  // char y = 'Y';
+  // char temp;
 
-  size_t rowsLen = sizeof(numbers) / sizeof(numbers[0]);
+  // temp = x;
+  // x = y;
+  // y = temp;
 
-  size_t columnsLen = sizeof(numbers[0]) / sizeof(numbers[0][0]);
+  // printf("x = %c \n", x);
+  // printf("y = %c \n", y);
 
+  char x[15] = "water";
+  char y[15] = "Lemonade";
+  char temp[15];
 
-  // for (int i = 0; i < rowsLen; i++)
-  // {
-  //   for (int j = 0; j < columnsLen; j++)
-  //   {
-  //     printf("%d ", numbers[i][j]);
-  //   }
+  strcpy(temp, x);
+  strcpy(x, y);
+  strcpy(y, temp);
 
-  //   printf("\n");
-
-  // }
-
- // * ARRAY OF STRINGS
-
-  char cars[][10] = { "Tesla", "Ferrari", "BMW" };
-
-  // !: how to reassign a string value inside an array
-  strcpy(cars[0], "Mustang");
-
-  size_t arrayLen = sizeof(cars) / sizeof(cars[0]);
-
-  for (int i = 0; i < arrayLen; i++)
-  {
-    printf("%s \n", cars[i]);
-  }
+  printf("x = %s \n", x);
+  printf("y = %s \n", y);
 
 
   return 0;

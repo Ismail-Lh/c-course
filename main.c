64a6b38639c1472b13488ecd91a05d6caef63201
Ismail-Lh <ismailsaad03@gmail.com>
2023-05-02 15:55:58 +0100

    number guessing game

--- main.c
+++ main.c
@@ -6,17 +6,42 @@
 enum Day { Sun, Mon, Tue, Wed, Thu, Fri, Sat };
 
 int main() {
-  // * PSEUDO RANDOM NUMBER -----> a set of values or elements that are statistically random (Don't use these for any sort of cryptographic security)
+  // * NUMBER GUESSING GAME
+
+  const int MIN = 1;
+  const int MAX = 100;
+
+  int guess;
+  int guesses;
+  int answer;
 
   srand(time(0));
 
-  int number1 = (rand() % 6) + 1;
-  int number2 = (rand() % 6) + 1;
-  int number3 = (rand() % 6) + 1;
+  // ?: generate a random number between MIN & MAX
+  answer = (rand() % MAX) + 1;
 
-  printf("%d \t", number1);
-  printf("%d \t", number2);
-  printf("%d", number3);
+  do
+  {
+    printf("Enter a guess: ");
+    scanf("%d", &guess);
+
+    if (guess > answer) {
+      printf("To high! \n");
+    }
+    else if (guess < answer) {
+      printf("To low! \n");
+    }
+    else {
+      printf("CORRECT!!! \n");
+    }
+    printf("\n");
+    guesses++;
+  } while (guess != answer);
+
+  printf("********************** \n");
+  printf("Answer: %d \n", answer);
+  printf("Guesses: %d \n", guesses);
+  printf("**********************");
 
   return 0;
 }

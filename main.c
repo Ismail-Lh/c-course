7d05a0de5058b3a927ceb9234dbfdac56b83e30e
Ismail-Lh <ismailsaad03@gmail.com>
2023-05-02 15:31:23 +0100

    generate random numbers

--- main.c
+++ main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 
 enum Day { Sun, Mon, Tue, Wed, Thu, Fri, Sat };
 
 int main() {
-  // * ENUMS ----> a user defined type of named integer identifiers helps to make a program more readable 
+  // * PSEUDO RANDOM NUMBER -----> a set of values or elements that are statistically random (Don't use these for any sort of cryptographic security)
 
-  enum Day today = Mon;
+  srand(time(0));
 
-  // !: enums are NOT STRINGS, but they can be treated as int
-  printf("%d", today);
+  int number1 = (rand() % 6) + 1;
+  int number2 = (rand() % 6) + 1;
+  int number3 = (rand() % 6) + 1;
+
+  printf("%d \t", number1);
+  printf("%d \t", number2);
+  printf("%d", number3);
 
   return 0;
 }

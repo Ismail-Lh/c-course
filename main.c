f581b6174fc027a4aa7d8eb8f988035c1e7b68f5
Ismail-Lh <ismailsaad03@gmail.com>
2023-05-02 18:01:52 +0100

    memory addresses

--- main.c
+++ main.c
@@ -6,50 +6,18 @@
 enum Day { Sun, Mon, Tue, Wed, Thu, Fri, Sat };
 
 int main() {
-  // * QUIZ GAME
+  // * MEMORY ----> an array of bytes within RAM (street)
+  // * MEMORY BLOCK -----> a single unit (byte) within a memory, used to hold some value (person)
+  // * MEMORY ADDRESS -----> the address of where a memory block is located (house address)
 
-  char questions[][100] = { "1. What year did the C language deb with creating C?: ", "2. Who is credited with creating C?: ", "3. What is the predecessor of C?: " };
+  char a;
+  char b[1];
 
-  char options[][100] = { "A. 1969", "B. 1972", "C. 1975", "D. 1999", "A. Dennis Ritchie", "B. Nicola Tesla", "C. John Carmack", "D. Doc B", "A. Objective C", "B. B", "C. C++", "D. C#", };
+  printf("%d bytes\n", sizeof(a));
+  printf("%d bytes\n", sizeof(b));
 
-  char answers[3] = { 'B', 'A', 'B' };
-
-  int numberOfQuestions = sizeof(questions) / sizeof(questions[0]);
-
-  char guess;
-  int score;
-
-  printf("*********** QUIZ GAME *********** \n");
-
-  for (int i = 0; i < numberOfQuestions; i++)
-  {
-    printf("********************* \n");
-    printf("%s \n", questions[i]);
-    printf("********************* \n");
-
-    for (int j = (i * 4); j < (i * 4) + 4; j++)
-    {
-      printf("%s \n", options[j]);
-    }
-
-    printf("guess: ");
-    scanf("%c", &guess);
-    scanf("%*c");
-
-    guess = toupper(guess);
-
-    if (guess == answers[i]) {
-      printf("CORRECT! \n");
-      score++;
-    }
-    else {
-      printf("WRONG! \n");
-    }
-  }
-
-  printf("********************* \n");
-  printf("FINAL SCORE: %d/%d \n", score, numberOfQuestions);
-  printf("********************* \n");
+  printf("%p\n", &a);
+  printf("%p\n", &b);
 
   return 0;
 }

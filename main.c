b30e6818846696c2b8e6d9df721a98b55405c99f
Ismail-Lh <ismailsaad03@gmail.com>
2023-05-02 16:43:22 +0100

    quiz game

--- main.c
+++ main.c
@@ -1,22 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <ctype.h>
 
 
 enum Day { Sun, Mon, Tue, Wed, Thu, Fri, Sat };
 
 int main() {
-  // * PSEUDO RANDOM NUMBER -----> a set of values or elements that are statistically random (Don't use these for any sort of cryptographic security)
+  // * QUIZ GAME
 
-  srand(time(0));
+  char questions[][100] = { "1. What year did the C language deb with creating C?: ", "2. Who is credited with creating C?: ", "3. What is the predecessor of C?: " };
 
-  int number1 = (rand() % 6) + 1;
-  int number2 = (rand() % 6) + 1;
-  int number3 = (rand() % 6) + 1;
+  char options[][100] = { "A. 1969", "B. 1972", "C. 1975", "D. 1999", "A. Dennis Ritchie", "B. Nicola Tesla", "C. John Carmack", "D. Doc B", "A. Objective C", "B. B", "C. C++", "D. C#", };
 
-  printf("%d \t", number1);
-  printf("%d \t", number2);
-  printf("%d", number3);
+  char answers[3] = { 'B', 'A', 'B' };
+
+  int numberOfQuestions = sizeof(questions) / sizeof(questions[0]);
+
+  char guess;
+  int score;
+
+  printf("*********** QUIZ GAME *********** \n");
+
+  for (int i = 0; i < numberOfQuestions; i++)
+  {
+    printf("********************* \n");
+    printf("%s \n", questions[i]);
+    printf("********************* \n");
+
+    for (int j = (i * 4); j < (i * 4) + 4; j++)
+    {
+      printf("%s \n", options[j]);
+    }
+
+    printf("guess: ");
+    scanf("%c", &guess);
+    scanf("%*c");
+
+    guess = toupper(guess);
+
+    if (guess == answers[i]) {
+      printf("CORRECT! \n");
+      score++;
+    }
+    else {
+      printf("WRONG! \n");
+    }
+  }
+
+  printf("********************* \n");
+  printf("FINAL SCORE: %d/%d \n", score, numberOfQuestions);
+  printf("********************* \n");
 
   return 0;
 }
